053748980c6e259b334c9dfd85c3e7e14adf03c7
surajgirioffl <surajkumargiri2002@gmail.com>
2022-01-23 10:46:55 +0530

    format the code and properly arrange declration for user ease

--- Header Files Used In this Repository/color.h
+++ Header Files Used In this Repository/color.h
@@ -1,94 +1,120 @@
 /* SIMPLE COLOR 3 bit*/
-#include<stdio.h>
-//argument ~ 'f' or 'F' for foreground and 'b' or 'B' for background
-//if user give wrong argument then foreground will work
-
-void black(char ch); //ch for f/F or b/B
+#include <stdio.h>
+/* argument ~ 'f' or 'F' for foreground and 'b' or 'B' for background
+if user give wrong argument then foreground will work*/
+void black(char ch); // ch for f/F or b/B
+/* argument ~ 'f' or 'F' for foreground and 'b' or 'B' for background
+if user give wrong argument then foreground will work*/
 void red(char ch);
+/* argument ~ 'f' or 'F' for foreground and 'b' or 'B' for background
+if user give wrong argument then foreground will work*/
 void green(char ch);
+/* argument ~ 'f' or 'F' for foreground and 'b' or 'B' for background
+if user give wrong argument then foreground will work*/
 void yellow(char ch);
+/* argument ~ 'f' or 'F' for foreground and 'b' or 'B' for background
+if user give wrong argument then foreground will work*/
 void blue(char ch);
+/* argument ~ 'f' or 'F' for foreground and 'b' or 'B' for background
+if user give wrong argument then foreground will work*/
 void magenta(char ch);
+/* argument ~ 'f' or 'F' for foreground and 'b' or 'B' for background
+if user give wrong argument then foreground will work*/
 void cyan(char ch);
+/* argument ~ 'f' or 'F' for foreground and 'b' or 'B' for background
+if user give wrong argument then foreground will work*/
 void white(char ch);
+/* argument ~ 'f' or 'F' for foreground and 'b' or 'B' for background
+if user give wrong argument then foreground will work*/
 void random_color();
 void advcolor(char ch, int n);
 
 void underline();
-void reset(); //to reset all text colors and back to normal
+void reset(); // to reset all text colors and back to normal
 
-//Definition part
-void black(char ch) 
+// Definition part
+void black(char ch)
 {
-  if(ch == 'b' || ch == 'B')
-  printf("\033[1;40m");
+  if (ch == 'b' || ch == 'B')
+    printf("\033[1;40m");
   else
-  printf("\033[1;30m");
+    printf("\033[1;30m");
 }
 
-void red(char ch) {
-  if(ch == 'b' || ch == 'B')
-  printf("\033[1;41m");
+void red(char ch)
+{
+  if (ch == 'b' || ch == 'B')
+    printf("\033[1;41m");
   else
-  printf("\033[1;31m");
+    printf("\033[1;31m");
 }
 
-void green(char ch) {
-  if(ch == 'b' || ch == 'B')
-  printf("\033[1;42m");
+void green(char ch)
+{
+  if (ch == 'b' || ch == 'B')
+    printf("\033[1;42m");
   else
-  printf("\033[1;32m");
+    printf("\033[1;32m");
 }
 
-void yellow(char ch) {
-  if(ch == 'b' || ch == 'B')
-  printf("\033[1;43m");
+void yellow(char ch)
+{
+  if (ch == 'b' || ch == 'B')
+    printf("\033[1;43m");
   else
-  printf("\033[1;33m");
+    printf("\033[1;33m");
 }
 
-void blue(char ch) {
-  if(ch == 'b' || ch == 'B')
-  printf("\033[1;44m");
+void blue(char ch)
+{
+  if (ch == 'b' || ch == 'B')
+    printf("\033[1;44m");
   else
-  printf("\033[1;34m");
+    printf("\033[1;34m");
 }
 
-void magenta(char ch) {
-  if(ch == 'b' || ch == 'B')
-  printf("\033[1;45m");
+void magenta(char ch)
+{
+  if (ch == 'b' || ch == 'B')
+    printf("\033[1;45m");
   else
-  printf("\033[1;35m");
+    printf("\033[1;35m");
 }
 
-void cyan(char ch) {
-  if(ch == 'b' || ch == 'B')
-  printf("\033[1;46m");
+void cyan(char ch)
+{
+  if (ch == 'b' || ch == 'B')
+    printf("\033[1;46m");
   else
-  printf("\033[1;36m");
+    printf("\033[1;36m");
 }
 
-void white(char ch) {
-  if(ch == 'b' || ch == 'B')
-  printf("\033[1;47m");
+void white(char ch)
+{
+  if (ch == 'b' || ch == 'B')
+    printf("\033[1;47m");
   else
-  printf("\033[1;37m");
+    printf("\033[1;37m");
 }
 
-void underline() {
+// to underline the test
+void underline()
+{
   printf("\033[4m");
 }
-void reset() {
+// call to reset the ANSI Color
+void reset()
+{
   printf("\033[0m");
 }
 
-
 /*taken from advcolor.h header file*/
 /* Advance 8 bit Colors combinations*/
-void advcolor(char ch, int n) //ch for background or foreground and n for color option and n must be in 0 to 255. ch must be 'b' or 'B' for background or any other character for foreground
+// ch for background or foreground and n for color option and n must be in 0 to 255. ch must be 'b' or 'B' for background or any other character for foreground
+void advcolor(char ch, int n)
 {
-  if((ch == 'b' || ch == 'B') && (n >= 0 && n < 256))
-  printf("\033[48;5;%dm", n);
+  if ((ch == 'b' || ch == 'B') && (n >= 0 && n < 256))
+    printf("\033[48;5;%dm", n);
   else
-  printf("\033[38;5;%dm", n);
+    printf("\033[38;5;%dm", n);
 }
